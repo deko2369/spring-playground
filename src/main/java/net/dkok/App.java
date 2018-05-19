dfa4c1cf9c51c195df1843430e56d4b65370f5dc
Kei Nishiyama <keinissy@gmail.com>
2018-05-19 16:00:36 +0900

    refactored App class

--- src/main/java/net/dkok/App.java
+++ src/main/java/net/dkok/App.java
@@ -5,11 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App
-{
-
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         SpringApplication app = new SpringApplication(App.class);
         app.setBannerMode(Banner.Mode.OFF);
         app.run(args);
