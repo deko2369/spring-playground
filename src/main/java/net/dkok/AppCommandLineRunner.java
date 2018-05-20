7c407f891a1d780d5413c74d51649cfdbe42a5bb
Kei Nishiyama <keinissy@gmail.com>
2018-05-20 12:26:18 +0900

    fixed profile to run test properly

--- src/main/java/net/dkok/AppCommandLineRunner.java
+++ src/main/java/net/dkok/AppCommandLineRunner.java
@@ -5,9 +5,11 @@ import net.dkok.ika.Ika;
 import net.dkok.ika.IkaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("!test")
 public class AppCommandLineRunner implements CommandLineRunner {
     private MySQLConfig config;
     private IkaRepository ikaRepository;
--- src/test/java/net/dkok/config/MySQLConfigTest.java
+++ src/test/java/net/dkok/config/MySQLConfigTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.CoreMatchers.*;
@@ -11,6 +12,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class MySQLConfigTest {
     @Autowired
     MySQLConfig config;
